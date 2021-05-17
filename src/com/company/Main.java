2a0dfa13602961160bb57f1f05b9ec9cc37c4326
randall.erasmus <randallerasmus1@gmail.com>
2021-05-17 23:45:24 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+//    Which options can fill in the blanks to print Cleaned 2 items?
+//
+//
+//            import java.util.*;
+//    class Wash˂T ________ Collection˃ {
+//        T item;
+//        public void clean(T items) {
+//            System.out.println("Cleaned " + items.size() + " items");
+//        }
+//    }
+//    public class LaundryTime {
+//        public static void main(String[] args) {
+//            Wash˂List˃ wash = new Wash˂_________________˃();
+//            wash.clean(List.of("sock", "tie"));
+//        }
+//    }
+//
+//    A. extends, ArrayList
+//    B. extends, List
+//    C. super, ArrayList
+//    D. super, List
+//    E. None of the above
+
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
